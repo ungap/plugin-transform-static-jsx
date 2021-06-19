760d2da6f44b7b23d5ef932b4bc6d21c8f74d6f2
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-06-19 14:12:31 +0200

    Avoid XML namespace hack and use a SOH  char instead

--- cjs/index.js
+++ cjs/index.js
@@ -1,6 +1,8 @@
 'use strict';
 const {escape} = require('html-escaper');
 
+const PREFIX = '\x01';
+
 /*! (c) Andrea Giammarchi & Nicolò Ribaudo - ISC */
 module.exports = ({types: t}) => ({
   visitor: {
@@ -8,13 +10,14 @@ module.exports = ({types: t}) => ({
       const {node: {name, value}, parent} = path;
 
       if (
+        name.name[0] === PREFIX ||
+        /^[A-Z]/.test(parent.name.name) ||
         t.isJSXNamespacedName(name) ||
-        t.isJSXExpressionContainer(value) ||
-        /^[A-Z]/.test(parent.name.name)
+        t.isJSXExpressionContainer(value)
       )
         return;
 
-      path.set('name', t.jsxNamespacedName(t.jsxIdentifier(''), name));
+      path.set('name', t.jsxIdentifier(PREFIX + name.name));
 
       if (t.isStringLiteral(value))
         value.value = escape(value.value);
--- test/output.js
+++ test/output.js
@@ -2,18 +2,18 @@ function Component({
   className
 }) {
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
-    ":id": "my-div",
+    "\x01id": "my-div",
     className: className
   }, /*#__PURE__*/React.createElement("p", {
     color: color,
-    ":label": "f&quot;o",
+    "\x01label": "f&quot;o",
     hidden: true
   })), /*#__PURE__*/React.createElement("div", {
-    ":id": "my-div",
+    "\x01id": "my-div",
     className: className
   }, /*#__PURE__*/React.createElement("p", {
     color: color,
-    ":label": "f&amp;o",
-    ":data-attr": "2"
+    "\x01label": "f&amp;o",
+    "\x01data-attr": "2"
   })));
 }
